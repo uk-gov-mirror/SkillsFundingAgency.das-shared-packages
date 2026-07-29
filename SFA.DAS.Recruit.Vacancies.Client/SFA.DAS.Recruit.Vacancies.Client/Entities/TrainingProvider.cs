03d1b5747562a530556cc6f09a0ab1bacab71555
killij <51908793+killij@users.noreply.github.com>
2026-07-29 16:01:00 +0100

    FAI-3184 Delete SFA.DAS.Recruit.Vacancies.Client

--- SFA.DAS.Recruit.Vacancies.Client/SFA.DAS.Recruit.Vacancies.Client/Entities/TrainingProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.Recruit.Vacancies.Client.Entities
-{
-    public class TrainingProvider
-    {
-        public long Ukprn { get; set; }
-        public string Name { get; set; }
-    }
-}
--- SFA.DAS.Recruit.Vacancies.Client/SFA.DAS.Recruit.Vacancies.Client/Entities/VacancyType.cs
+++
@@ -1,8 +0,0 @@
-namespace SFA.DAS.Recruit.Vacancies.Client.Entities
-{
-    public enum VacancyType
-    {
-        Apprenticeship,
-        Traineeship
-    }
-}
