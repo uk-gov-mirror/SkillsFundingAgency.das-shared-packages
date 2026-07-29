283c6df298829ec984a1794966bdc7cb2e1e71bf
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-07-29 10:12:00 +0100

    Remove all source, test, and config files
    All C# source files, tests, project/solution files, and configuration files have been deleted. The repository is now empty.

--- SFA.DAS.VacancyServices.Search/SFA.DAS.VacancyServices.Search/ILocationSearchClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.VacancyServices.Search
-{
-    using Responses;
-
-    public interface ILocationSearchClient
-    {
-        LocationSearchResponse Search(string placeName, int maxResults = LocationSearchClient.MaxResults);
-    }
-}
